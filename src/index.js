5467fe6a78dccc4f59aaf18b402e148cf93d0b92
ulmasbek <[rakhmatullaevulmas@gmail.com]>
2020-02-24 22:47:50 +0500

    feature: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
+exports.min = function min(array) {
 
-exports.min = function min (array) {
-  return 0;
+    if (!array || array.length === 0)
+        return 0;
+    var min = array[0];
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] < min)
+            min = array[i]
+    }
+    return min;
+
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!array || array.length === 0)
+        return 0;
+    var max = array[0];
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] > max)
+            max = array[i]
+    }
+    return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!array || array.length === 0)
+        return 0;
+    var sum = 0;
+    for (var i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum / array.length;
 }
